bba964d0b8d3b1f6ee8adf676522dc59fd2b2415
digital <digital@iMac24-5.local>
2022-11-30 09:25:16 +0100

    ajout d'une collectionview

--- collectionView/ViewController.swift
+++ collectionView/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collecView: UICollectionView!
+    
+    var data = ["1","2","3","4","5"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.collecView.delegate = self
+        self.collecView.dataSource = self
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collecView.dequeueReusableCell(withReuseIdentifier: "protoCell", for: indexPath)
+        
+        cell.backgroundColor = UIColor.red
+        //cell.layer.borderColor = CGColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
+        
+        return cell
+    }
+    
+    
 
-
+    
 }
 
