5c55cd3601c3cf8f6cc7b8b1f0be1556f86aabaf
digital <digital@iMac24-5.local>
2022-11-30 09:38:25 +0100

    Ajoout de la taille des cellules

--- collectionView/ViewController.swift
+++ collectionView/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collecView: UICollectionView!
     
@@ -30,6 +30,23 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         //cell.layer.borderColor = CGColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1)
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var size = CGSize()
+        
+        size.width = self.collecView.frame.width/3 - 3
+        size.height = size.width
+        
+        return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(1.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(1.0)
     }
     
     
